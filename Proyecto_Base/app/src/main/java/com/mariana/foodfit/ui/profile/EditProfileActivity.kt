accf87fc2c79486c1c01a9bd6df52c6cdd2d477a
MichaelGarciaJ <mgarcia271476@gmail.com>
2025-05-02 12:59:00 +0200

    EditProfileActivity
    - Se ha añadido el layout de activity_edit_profile.xml

--- Proyecto_Base/app/src/main/java/com/mariana/foodfit/ui/profile/EditProfileActivity.kt
+++ Proyecto_Base/app/src/main/java/com/mariana/foodfit/ui/profile/EditProfileActivity.kt
@@ -6,16 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mariana.foodfit.R
+import com.mariana.foodfit.databinding.ActivityEditProfileBinding
+import com.mariana.foodfit.databinding.ActivityHomeBinding
+import com.mariana.foodfit.utils.ToolbarUtils
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Configuramos el botón de menú para abrir/cerrar el drawer
+        ToolbarUtils.configurarDrawerToggle(binding.editProfileCustomToolbar, binding.editProfileDrawerLayout)
+
     }
 }
