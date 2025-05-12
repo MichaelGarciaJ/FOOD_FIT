0add7729f197898fda7ff58d43d2b0a7db436fbd
MichaelGarciaJ <mgarcia271476@gmail.com>
2025-05-12 13:54:33 +0200

    Vista Detalla Comida
    - Se está implementado la vista donde se verá los datos del platillo.

---
+++ Proyecto_Base/app/src/main/java/com/mariana/foodfit/ui/meals/foodPlateInfo/FoodPlateInfoActivity.kt
@@ -0,0 +1,21 @@
+package com.mariana.foodfit.ui.meals.foodPlateInfo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mariana.foodfit.R
+
+class FoodPlateInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_food_plate_info)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
